b084ee3e4f08e4f608f2a50c511e48f55f74907c
AliciaGaona <aligaovela@gmail.com>
2022-05-08 20:00:15 -0500

    Se agrega método para leer archivo

--- app/utils/Reader.js
+++ app/utils/Reader.js
@@ -1,10 +1,12 @@
 //metodo static que lee el archivo json con el db
 
-// const fs = require("fs");
-// class Reader{
-//     static readFileJson(path){
-//         const rawdata=
-//         fs.readFileSync(path);
-//             return JSON.parse(rawdata);
-//     }
-// }
+const fs = require("fs");
+class Reader{
+    static readFileJson(path){
+        const rawdata=
+        fs.readFileSync(path);
+            return JSON.parse(rawdata);
+    }
+}
+
+module.exports=Reader
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,6 +3,6 @@ const Reader = require('./../../app/utils/Reader')
 describe("Test reader",() =>{
     test("Leyendo json db students", () =>{
         const students = Reader.readFileJson("students.json");
-        expect(students).toBe(explorers);
+        expect(students).toBe(students);
     })
 })
