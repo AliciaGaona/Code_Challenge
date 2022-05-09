af632a7387ecf020844ba0f94bc965080173a63b
AliciaGaona <aligaovela@gmail.com>
2022-05-08 20:08:18 -0500

    Se crea prueba unitaria para metodo getStudents

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,13 @@
+const StudentServer = require('./../../app/services/StudentService')
+
+//Habilitar un endpoint para consultar todos los estudiantes que tengan credits mayor a 500.
+//endpoint para consultar los emails de todos los estudiantes que tengan certificación haveCertification.
+//endpoint para consultar todos los estudiantes con todos sus campos.
+
+describe("Test para servicios StudentServer", () =>{
+    test("Prueba unitaria para primer requerimiento, metodo que trae todos los estudiantes", () =>{
+        const getStudents= StudentServer.getStudents();
+        expect(getStudents).not.toBeUndefined();
+
+    }) 
+})
