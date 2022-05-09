c407e140d0bf8aa7e8226f374ba006e0eb3b4018
AliciaGaona <aligaovela@gmail.com>
2022-05-09 02:02:58 -0500

    Se corrige referencia de controller

--- app/server.js
+++ app/server.js
@@ -5,18 +5,18 @@ app.use(express.json());
 const port= 3000;
 
 app.get("/v1/students/all", (request, response) => {
-    const studentsAll = ExplorerController.getStudents();
+    const studentsAll = StudentsController.getStudents();
     response.json(explorersInMission);
 });
 
 app.get("/v1/students/Email/haveCertification", (request, response) => {
-    const studentsHaveCertification = ExplorerController.getStudentsFilterEmail();
+    const studentsHaveCertification = StudentsController.getStudentsFilterEmail();
     response.json(studentsHaveCertification);
 });
 
 
 app.get("/v1/students/credits", (request, response) => {
-    const studentsCredits = ExplorerController.getStudentsFilterCreditos();
+    const studentsCredits = StudentsController.getStudentsFilterCreditos();
     response.json(studentsCredits);
 });
 
