e615bc736c696818d693feffd3b77427552db09f
AliciaGaona <aligaovela@gmail.com>
2022-05-09 01:07:47 -0500

    Se modifica prueba unitaria para requerimeinto 2, controller

--- test/controller/StudentsController.test.js
+++ test/controller/StudentsController.test.js
@@ -3,11 +3,11 @@ const StudentsController= require("../../app/controller/StudentsController")
   test("Prueba unitaria para primer requerimiento, metodo que trae todos los estudiantes controller", () =>{
     const getStudents= StudentsController.getStudents();
     expect(getStudents).not.toBeUndefined();
+  });
 
-    // test("Prueba unitaria segundo requrimiento getStudentsFilterEmail, controller ", () =>{
-    //   const students= ({ email: "Sharlene@visualpartnership.xyz",haveCertification: true});
-    //   const getStudents= StudentsController.getStudents(students);
-    //   expect(getStudents.lenght).toBe(1);
-    // });
+    test("Prueba unitaria segundo requrimiento getStudentsFilterEmail, controller ", () =>{
+      const students= ({ email: "Sharlene@visualpartnership.xyz",haveCertification: true});
+      const getStudents= StudentsController.getStudents(students);
+      expect(getStudents).not.toBeUndefined();
+    });
     
-}) 
