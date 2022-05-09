3ccbf3c33f30453780f2b9fbab6486b5d3d51e27
AliciaGaona <aligaovela@gmail.com>
2022-05-08 23:57:07 -0500

    Se agrega metodo para pasar oprueba unitaria de requerimiento2, pausa 8:34 domingo 8 de mayo 2022

--- test/controller/StudentsController.test.js
+++ test/controller/StudentsController.test.js
@@ -3,4 +3,10 @@ const StudentsController= require("../../app/controller/StudentsController")
   test("Prueba unitaria para primer requerimiento, metodo que trae todos los estudiantes controller", () =>{
     const getStudents= StudentsController.getStudents();
     expect(getStudents).not.toBeUndefined();
+
+    test("Prueba unitaria segundo requrimiento getStudentsFilterEmail, controller ", () =>{
+    const getStudentsFilterEmail=StudentsController.getStudentsFilterEmail("haveCertification");
+    expect(getStudentsFilterEmail).toBeDefined();
+    })
+    
 }) 
