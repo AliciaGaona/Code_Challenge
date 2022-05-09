53b11d88edf4c5044938248c28705d804629ae9e
AliciaGaona <aligaovela@gmail.com>
2022-05-09 02:05:07 -0500

    Se corrige variable de salida, nota no copiar y pegar nada!

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const port= 3000;
 
 app.get("/v1/students/all", (request, response) => {
     const studentsAll = StudentsController.getStudents();
-    response.json(explorersInMission);
+    response.json(studentsAll);
 });
 
 app.get("/v1/students/Email/haveCertification", (request, response) => {
